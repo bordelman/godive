56422548a51ad05c7fbf4c7121e3e2dd7916653c
Jakub Peca <jakub.peca@orgis.cz>
2021-08-08 09:42:54 +0200

    wip

--- formEvaluation.php
+++ formEvaluation.php
@@ -13,35 +13,43 @@
 
 <body>
     <main>
-        <h1>Děkujeme za přihlášení na kurz
-					<?php
-				$courses = [
-					"trialDive" => "Ponor na zkoušku",
-					"owd" => "OWD",
-					"aowd" => "AOWD",
-					"nd" => "Nitrox Diver",
-					"dd" => "Deep Diver",
-					"and" => "Advanced Nitrox Diver",
-					"artd" => "Advanced Recreational Trimix Diver",
-					"ntd" => "Normoxic Trimix Diver",
-					"td" => "Trimix Diver",
-					"rd" => "Rescue Diver",
-					"dm" => "Divemaster",
-					"id" => "Ice Diver",
-					"dsd" => "Dry Suit Diver",
-				];
-				echo $courses[$_POST["course"]];
-				?></h1>
-				<p>Na váš e-mail: <?php echo $_POST["email"];?> byla odeslána kopie vaší přihlášky</p>
-				<?php
-				$body = "";
-				foreach($_POST as $item){
-					echo $item;
-					$body .= $item;
-				}
-				echo "\nmrdat\n\n";
-				echo var_dump($body);
-				mb_send_mail($_POST["email"], "Prdel", $body);
+		<?php
+			mb_internal_encoding("UTF-8");
+
+			if ($_POST["botControl"] != date("Y")) {
+				echo "<h1 style=\"color:red\"> Bohužel, konrolní otázka nebyla zodpovězena správně. Zkuste to znova!</h1>";
+			} else {
+			$courses = [
+				"trialDive" => "Ponor na zkoušku",
+				"owd" => "OWD",
+				"aowd" => "AOWD",
+				"nd" => "Nitrox Diver",
+				"dd" => "Deep Diver",
+				"and" => "Advanced Nitrox Diver",
+				"artd" => "Advanced Recreational Trimix Diver",
+				"ntd" => "Normoxic Trimix Diver",
+				"td" => "Trimix Diver",
+				"rd" => "Rescue Diver",
+				"dm" => "Divemaster",
+				"id" => "Ice Diver",
+				"dsd" => "Dry Suit Diver",
+			];
+			echo "<h1>Děkujeme za přihlášení na kurz ".$courses[$_POST["course"]]."</h1>";
+
+				$body = "vaše ulice je: ".$_POST["street"];
+				
+				$header = 'From:GoDive //<info@godive.cz>;';
+$header .= "\nMIME-Version: 1.0\n";
+$header .= "Content-Type: text/html; charset=\"utf-8\"\n";
+$subject = 'Nová zpráva z mailformu';
+				echo "<br>".$body."<br>";
+				$succes = mb_send_mail($_POST["email"], $subject, $body);//, $header);
+				echo "<br>odesláno ".$succes;
+				if ($succes)
+				echo "<p>Na váš e-mail: ".$_POST["email"]." byla odeslána kopie vaší přihlášky</p>";
+				else 
+					echo "<p>E-mail se nepodařilo odeslat, kontaktujte nás prosím přímo na <a href=\"tel:+420 602 148 026\">+420 602 148 026</a></p>";
+		}
 				?>
 
     </main>
