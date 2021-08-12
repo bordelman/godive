7812b2045c8930304773b4424d12ae361d9c8609
Jakub Peca <pecaj.dev@gmail.com>
2021-08-12 20:52:37 +0200

    Fuck that fucking PDF

--- formEvaluation.php
+++ formEvaluation.php
@@ -15,11 +15,13 @@
 <body>
 	<main class="thankUPage">
 		<div class="content">
-			<?php
-			mb_internal_encoding("UTF-8");
+			<?= "<div class=\"content\">";
 			if ($_POST["botControl"] != date("Y")) {
 				echo "<h1 style=\"color:red\"> Bohužel, konrolní otázka nebyla zodpovězena správně. Zkuste to, prosím, znova!</h1>";
 			} else {
+				$date = new DateTime($_POST["birthDate"]);
+				$date = $date->format('d.m.Y');
+				$url = "http://www.concept.godive.cz/registrationPrint.php?";
 				$courses = [
 					"trialDive" => "Ponor na zkoušku",
 					"owd" => "OWD",
@@ -35,180 +37,93 @@
 					"id" => "Ice Diver",
 					"dsd" => "Dry Suit Diver",
 				];
-				echo "<h1>Děkujeme za přihlášení na kurz<br>" . $courses[$_POST["course"]] . "</h1>";
+				$response = [
+					"course" => $_POST["course"],
+					"firstName" => $_POST["firstName"],
+					"lastName" => $_POST["lastName"],
+					"street" => $_POST["street"],
+					"zip" => $_POST["zip"],
+					"state" => $_POST["state"],
+					"city" => $_POST["city"],
+					"email" => $_POST["email"],
+					"phone" => $_POST["phone"],
+					"member" => $_POST["member"],
+					"memberId" => $_POST["memberId"],
+					"date" => $date
+				];
+				$urlParams = [];
+				foreach ($response as $key => $value) {
+					array_push($urlParams, $key . "=" . urlencode($value));
+				}
+				$url .= implode("&", $urlParams);
+				mb_internal_encoding("UTF-8");
+				$message = "Děkujeme za vaší přihlášku, pokud jste si přihlášku nestáhli/nevytsiknuli, najdete ji na tomto odkazu:\n <b><a href=$url>Přihláška</a></b>";
+				$header = 'From:GoDive@godive.cz;';
+				$header .= "\nMIME-Version: 1.0\n";
+				$header .= "Content-Type: text/html; charset=\"utf-8\"\n";
+				$subject = "Potvrzení přijetí přihlášky";
+				$succes = mb_send_mail($_POST["email"], $subject, $message, $header);
+				if (!$succes)
+					echo "potvrzovací email neodešel";
+				echo "<h1>Děkujeme za přihlášení na kurz<br>" . $courses[$_POST["course"]] . "</h1>
+				</div>
+				<div class=\"content\">
+				<p>Vytiskněte si prosím přihlášku, podepište a přineste na první hodinu</p>
+				<div class=\"buttons\">
+				<a class=\"btn\" href=$url target=\"_blank\">Vytisknout přihlášku</button></a>
+				</div>";
+				$conditions = [
+					"alkoholProblem" => "Alkohol",
+					"diabetesProblem" => "Cukrovka",
+					"smokingProblem" => "Kouření",
+					"earsProblem" => "Problém s ušima",
+					"claustrofobiaProblem" => "Klaustrofobie",
+					"recentSurgeryProblem" => "Operace",
+					"headacheProblem" => "Bolesti hlavy",
+					"depressionProblem" => "Deprese",
+					"dizzinessProblem" => "Závratě",
+					"digestingProblem" => "Problémy s trávením",
+				];
+				$overallCondition = [];
+				foreach ($conditions as $key => $value) {
+					if ($_POST[$key] == "true") {
+						array_push($overallCondition, $value);
+					}
+				}
+				$criticalConditions = [
+					"breathingProblems" => "Dýchání",
+					"balanceProblem" => "Rovnováha",
+					"asthmaProblem" => "Astma",
+					"epilepsyProblem" => "Epilepsie",
+					"drugsProblem" => "Léky",
+					"cardiacProblem" => "Srdce",
+					"pregnancyProblem" => "Těhotenství"
+				];
+
+				$overallCriticalCondition = [];
+				foreach ($criticalConditions as $key => $value) {
+					if ($_POST[$key] == "true") {
+						array_push($overallCriticalCondition, $value);
+					}
+				}
+				if (count($overallCriticalCondition) > 0) echo "<p class=\"warning\">Z důvodu zdravotních komplikací (" . implode(", ", $overallCriticalCondition) . ") bude pro účast vyžadován souhlas lékaře!</p>";
+				$message = "<h1>Nově zaregistrovaný <a href=$url>uživatel</a> na kurz " . $courses[$_POST["course"]] . "</h1>";
+				$message .= "<p>Jméno: ${_POST["firstName"]} ${_POST["lastName"]}, mobil: ${_POST["phone"]}, email: ${_POST["email"]} </p>";
+				$message .= "<p>Výška: ${_POST["height"]}, váha: ${_POST["weight"]}, velikost boty: ${_POST["shoeSize"]}</p>";
+				$message .= "<p>IANTD člen: " . ($_POST["member"] == "true" ? "ANO - " . $_POST["memberId"] : "NE") . "</p>";
+				$message .= "<p>Udané problémy ve formuláři: " . implode(", ", $overallCondition) . "</p>";
+				$message .= "<p>Udané závažné problémy ve formuláři: " . implode(", ", $overallCriticalCondition) . "</p>";
+				$header = 'From:GoDive@godive.cz;';
+				$header .= "\nMIME-Version: 1.0\n";
+				$header .= "Content-Type: text/html; charset=\"utf-8\"\n";
+				$subject = "Informace o nové přihlášce" .  $courses[$_POST["course"]];
+				$succes = mb_send_mail($_POST["email"], $subject, $message, $header);
+				if (!$succes)
+					echo "<p>email do GoDive neodešel, kontaktujte nás prosím na telefonu: <a href='tel:+420 602 148 026'>+420 602 148 026</p>";
 			}
 			?>
 		</div>
-		<?php
-		if ($_POST["botControl"] == date("Y")) {
-			$conditions = [
-				"breathingProblems" => "Dýchání",
-				"balanceProblem" => "Rovnováha",
-				"alkoholProblem" => "Alkohol",
-				"diabetesProblem" => "Cukrovka",
-				"asthmaProblem" => "Astma",
-				"smokingProblem" => "Kouření",
-				"epilepsyProblem" => "Epilepsie",
-				"earsProblem" => "Uši",
-				"drugsProblem" => "Léky",
-				"cardiacProblem" => "Srdce",
-				"claustrofobiaProblem" => "Klaustrofobie",
-				"recentSurgeryProblem" => "Operace",
-				"headacheProblem" => "Hlava",
-				"depressionProblem" => "Deprese",
-				"dizzinessProblem" => "Závratě",
-				"digestingProblem" => "Trávení",
-				"pregnancyProblem" => "Těhotenství"
-			];
-			$date = new DateTime($_POST["birthDate"]);
-			$date = $date->format('d.m.Y');
-			$lineSpace = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-			$courses = ["trialDive" => "Ponor na zkoušku", "owd" => "OWD", "aowd" => "AOWD", "nd" => "Nitrox Diver", "dd" => "Deep Diver", "and" => "Advanced Nitrox Diver", "artd" => "Advanced Recreational Trimix Diver", "ntd" => "Normoxic Trimix Diver", "td" =>
-			"Trimix Diver", "rd" => "Rescue Diver", "dm" => "Divemaster", "id" => "Ice Diver", "dsd" => "Dry Suit Diver",];
-			$message =  "
-		<head>
-	<style>
-	.page * {
-		text-shadow:none;
-	color:black}
-	.page {
-		font-family: sans-serif;
-		width: 21cm;
-		height: 29.7cm;
-		padding: 30mm 45mm 30mm 45mm;
-		margin: auto
-	}
-	
-	.page header p, h3 {
-		margin: auto;
-	}
-	
-	.headerText.column {
-		display:flex;
-		flex-direction: column;
-
-	.pageHeader {
-		display: flex;
-		justify-content: space-around;
-	}
-	
-	.page header img {
-		height: 150px;
-		width: auto;
-	}
-	
-	.line {
-		text-decoration: underline;
-	}
-	
-	.underaged {
-		display: flex;
-		flex-direction: row;
-		text-align: center;
-	}
-	
-	.parent {
-		width: 55ch;
-		margin:auto
-	}
-	
-	.sign {
-		width: 30ch;
-		margin: auto
-	}
-	</style>
-</head>
-					<div class=\"page\">
-			<header class=\"pageHeader\" style=\"display:flex; flex-direction:row; justify-content: space-around;\">
-				<img src=\"http://www.concept.godive.cz/sources/logo.png\" alt=\"godive logo\">
-				<div class=\"headerText column\">
-					<p>Instruktor: IANTD #9801 Ladislav Hájek www.godive.cz</p>
-					<h3>Přihláška do kurzu a přihláška k členství</h3>
-				</div class=\"headerText\">
-				<img src=\"http://www.concept.godive.cz/sources/logoIantd.png\" alt=\"IANTD logo\">
-			</header>
-			<div id=\"personalInfo\">
-			<p>Jméno: <b>${_POST["firstName"]} ${_POST["lastName"]}</b>&nbsp;Datum narození: <b>$date</b></p>
-			<p>Adresa: <b>${_POST["street"]}, ${_POST["city"]}</b></p>
-			<p>PSČ: <b>${_POST["zip"]}</b> Stát: <b>${_POST["state"]}</b></p>
-			<p>Email: <b>${_POST["email"]}</b> telefon: <b>${_POST["phone"]}</b></p>
-			<h3>POŽADOVANÝ KURZ/kvalifikace: <b>${courses[$_POST["course"]]}</b></h3>
-			<p>Já níže podepsaný, se přihlašuji do označeného kurzu potápění vedeného instruktorem - členem IANTD <br> a k
-				členství v IANTD.</p>
-				<p>Prohlašuji, že již jsem kvalifikovaný potápěč a že jsem svoji kvalifikaci
-				<span class=\"line\">$lineSpace</span> získal/a u následující organizace
-				<span class=\"line\">$lineSpace</span>, což dokládám.</p>
-			<p>Jsem již členem IANTD
-				<input type=\"checkbox\" id=member " . ($_POST["member"] == "true" ? "checked" : "") . " disabled> <label>ANO</label>
-        <input type=\"checkbox\" id=member " . ($_POST["member"] == "false" ? "checked" : "") . " disabled> <label>NE</label></p>
-			<p>Jsem srozuměn s tím, že aktivace mého členství v IANTD i aktivace platnosti mé kvalifikace je podmíněna
-				úspěšným absolvováním kurzu a přijetím smluních podmínek členství v IANTD , které mne opravňuje k prokazování
-				mnou dosažené potápěčské
-				IANTD kvalifikace vůči třetím osobám. Dále beru na vědomí, že platný průkaz člena IANTD stvrzující moji
-				kvalifikaci mi bude vystaven po absolvování kurzu a po úhradě příslušného poplatku prostřednictvím IANTD Central
-				Europe s.r.o..</p>
-			
-			<p>Podpisem této přihlášky zároveň stvrzuji přijetí závazků vyplývajících z mnou současně podepsancýh Podmínek členství. Svým podpisem této přihlášky výslovně potvrzuji, že:</p>
-			<ul>
-				<li>výše uvedené údaje jsou dle mého vědomí správné a úplně</li>
-				<li>podmínky účasti v kurzu a členství v IANTD jsem si přečetl/-a jejich obsah plně akceptuji a souhlasím s ním</li>
-				<li>splňuji podmínky pro účast v kurzu</li>
-				<li>výslovně žádám, aby mé jméno, příjmení, datum narození, jakožto člena IANTD byly prostřednictvím instruktora a IANTD Central Europe s.r.o. předány IANTD/IAND Inc. a uvedeny na seznamu členů na webových stránkách IANTD, které obsahují seznam členů IANTD s vyznačením příslušné dosažené kvalifikace a č. IANTD</li>
-				<li>v rámci kurzu se zavazuji respektovat pokyny instruktora</li>
-				<li>potvrzuji, že jsem se před podpisem této listiny seznámil s Informací o opatřeních na ochranu osobních údajů instruktora / facility na jeho/ jejich www. stránkách a na stránkách IANTD Central Europe s.r.o. www.iantd.cz</li>
-			</ul>
-			<p>Datum: <span class=\"line\">$lineSpace</span></p>
-			<p>Podpis: <span class=\"line\">$lineSpace</span>&nbsp;IANTD č.: <b>${_POST["memberId"]}</b></p>
-			<p>Za nezletilého účastníka též zákonný zástupce:</p>
-			<div class=\"underaged\">
-				<p class=\"parent line\">$lineSpace$lineSpace$lineSpace$lineSpace</p>
-				<p class=\"sign line\">$lineSpace</p>
-			</div>
-			<div class=\"underaged\">
-				<p class=\"parent\">jméno, příjmení, datum narození, vztah k nezletilému</p>
-				<p class=\"sign\">podpis</p>
-			</div>
-		</div>
-		</div>
-		</main>
-		</body>";
-
-			$header = 'From:GoDive@godive.cz;';
-			$header .= "\nMIME-Version: 1.0\n";
-			$header .= "Content-Type: text/html; charset=\"utf-8\"\n";
-			$subject = "Potvrzení přijetí přihlášky-asd";
-			$succes = mb_send_mail($_POST["email"], $subject, $message, $header);
-			echo " <div class=\"content\">
-			<p>Vytiskněte si prosím přihlášku, podepište a přineste na první hodinu</p>
-			<div class=\"buttons\">
-				<button class=\"btn\" type=\"button\" onclick=\"window.print()\">Vytisknout přihlášku</button>
-			</div>";
-			if (
-				$_POST["breathingProblems"] == "true" ||
-				$_POST["balanceProblem"] == "true" ||
-				$_POST["asthmaProblem"] == "true" ||
-				$_POST["epilepsyProblem"] == "true" ||
-				$_POST["drugsProblem"] == "true" ||
-				$_POST["cardiacProblem"] == "true" ||
-				$_POST["pregnancyProblem"] == "true"
-			)
-				echo "<p class=\"warning\">Z důvodu zdravotních komplikací bude pro účast vyžadován souhlas lékaře!</p>";
-		}
-
-		$otherInfo = "Výška: " . $_POST["height"] . "<br>";
-		$otherInfo .= "Váha: " . $_POST["weight"] . "<br>";
-		$otherInfo .= "Velikost boty: " . $_POST["shoeSize"] . "<br>";
-
-		foreach ($conditions as $key => $condition) {
-			if ($_POST[$key] == "true")
-				$otherInfo .= "POZOR! " . $condition . "<br>";
-		}
-		?>
-		</div>
 	</main>
-	<div class="print">
-		<?= $message ?>
-	</div>
 </body>
 
 </html>
---
+++ registrationPrint.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel='stylesheet' href='css/confirmation.css'>
+	<title>Tisk přihlášky</title>
+</head>
+
+<body>
+	<main>
+		<?php
+
+		$lineSpace = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+		$courses = [
+			"trialDive" => "Ponor na zkoušku",
+			"owd" => "OWD",
+			"aowd" => "AOWD",
+			"nd" => "Nitrox Diver",
+			"dd" => "Deep Diver",
+			"and" => "Advanced Nitrox Diver",
+			"artd" => "Advanced Recreational Trimix Diver",
+			"ntd" => "Normoxic Trimix Diver",
+			"td" => "Trimix Diver",
+			"rd" => "Rescue Diver",
+			"dm" => "Divemaster",
+			"id" => "Ice Diver",
+			"dsd" => "Dry Suit Diver",
+		];
+		echo "
+		<div class=\"page\">
+			<header class=\"pageHeader\">
+				<img src=\"http://www.concept.godive.cz/sources/logo.png\" alt=\"godive logo\">
+				<div class=\"headerText column\">
+					<p>Instruktor: IANTD #9801 Ladislav Hájek www.godive.cz</p>
+					<h3>Přihláška do kurzu a přihláška k členství</h3>
+				</div class=\"headerText\">
+				<img src=\"http://www.concept.godive.cz/sources/logoIantd.png\" alt=\"IANTD logo\">
+			</header>
+			<div id=\"personalInfo\">
+				<p>Jméno: <b>${_GET["firstName"]} ${_GET["lastName"]}</b>&nbsp;Datum narození: <b>${_GET["date"]}</b></p>
+				<p>Adresa: <b>${_GET["street"]}, ${_GET["city"]}</b></p>
+				<p>PSČ: <b>${_GET["zip"]}</b> Stát: <b>${_GET["state"]}</b></p>
+				<p>Email: <b>${_GET["email"]}</b> telefon: <b>${_GET["phone"]}</b></p>
+				<h3>POŽADOVANÝ KURZ/kvalifikace: <b>${courses[$_GET["course"]]}</b></h3>
+				<p>Já níže podepsaný, se přihlašuji do označeného kurzu potápění vedeného instruktorem - členem IANTD <br> a k
+					členství v IANTD.</p>
+				<p>Prohlašuji, že již jsem kvalifikovaný potápěč a že jsem svoji kvalifikaci
+					<span class=\"line\">$lineSpace</span> získal/a u následující organizace
+					<span class=\"line\">$lineSpace</span>, což dokládám.
+				</p>
+				<p>Jsem již členem IANTD
+					<input type=\"checkbox\" id=member " . ($_GET["member"] == "true" ? "checked" : "") . " disabled> <label>ANO</label>
+        <input type=\"checkbox\" id=member " . ($_GET["member"] == "false" ? "checked" : "") . " disabled> <label>NE</label></p>
+			<p>Jsem srozuměn s tím, že aktivace mého členství v IANTD i aktivace platnosti mé kvalifikace je podmíněna
+				úspěšným absolvováním kurzu a přijetím smluních podmínek členství v IANTD , které mne opravňuje k prokazování
+				mnou dosažené potápěčské
+				IANTD kvalifikace vůči třetím osobám. Dále beru na vědomí, že platný průkaz člena IANTD stvrzující moji
+				kvalifikaci mi bude vystaven po absolvování kurzu a po úhradě příslušného poplatku prostřednictvím IANTD Central
+				Europe s.r.o..</p>
+
+			<p>Podpisem této přihlášky zároveň stvrzuji přijetí závazků vyplývajících z mnou současně podepsancýh Podmínek členství. Svým podpisem této přihlášky výslovně potvrzuji, že:</p>
+			<ul>
+				<li>výše uvedené údaje jsou dle mého vědomí správné a úplně</li>
+				<li>podmínky účasti v kurzu a členství v IANTD jsem si přečetl/-a jejich obsah plně akceptuji a souhlasím s ním</li>
+				<li>splňuji podmínky pro účast v kurzu</li>
+				<li>výslovně žádám, aby mé jméno, příjmení, datum narození, jakožto člena IANTD byly prostřednictvím instruktora a IANTD Central Europe s.r.o. předány IANTD/IAND Inc. a uvedeny na seznamu členů na webových stránkách IANTD, které obsahují seznam členů IANTD s vyznačením příslušné dosažené kvalifikace a č. IANTD</li>
+				<li>v rámci kurzu se zavazuji respektovat pokyny instruktora</li>
+				<li>potvrzuji, že jsem se před podpisem této listiny seznámil s Informací o opatřeních na ochranu osobních údajů instruktora / facility na jeho/ jejich www. stránkách a na stránkách IANTD Central Europe s.r.o. www.iantd.cz</li>
+			</ul>
+			<p>Datum: <span class=\" line\">$lineSpace</span>
+				</p>
+				<p>Podpis: <span class=\"line\">$lineSpace</span>&nbsp;IANTD č.: ". (intval($_GET["memberId"]) > 0 ? "<b>${_GET["memberId"]}</b>" : "<span class=\"line\">$lineSpace</span>")."</p>
+				<p>Za nezletilého účastníka též zákonný zástupce:</p>
+				<div class=\"underaged\">
+					<p class=\"parent line\">$lineSpace$lineSpace$lineSpace$lineSpace</p>
+					<p class=\"sign line\">$lineSpace</p>
+				</div>
+				<div class=\"underaged\">
+					<p class=\"parent\">jméno, příjmení, datum narození, vztah k nezletilému</p>
+					<p class=\"sign\">podpis</p>
+				</div>
+			</div>
+		</div>
+		";
+		?>
+	</main>
+</body>
